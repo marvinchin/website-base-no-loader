36555ef59d3492b4e07fa09cf030237d533718fe
marvinchin <chinkunsong.marvin@gmail.com>
2019-01-26 12:38:33 +0800

    Site Update.

--- markbind/js/setup.js
+++ markbind/js/setup.js
@@ -33,10 +33,6 @@ function setupAnchors() {
   });
 }
 
-function removeLoadingOverlay() {
-  jQuery('#loading-overlay').remove();
-}
-
 function updateSearchData(vm) {
   jQuery.getJSON(`${baseUrl}/siteData.json`)
     .then((siteData) => {
@@ -49,7 +45,6 @@ function executeAfterMountedRoutines() {
   flattenModals();
   scrollToUrlAnchorHeading();
   setupAnchors();
-  removeLoadingOverlay();
 }
 
 function setupSiteNav() {
